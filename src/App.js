9b3ae3ddcb70026f733900d1d100fb5b75578792
Connor <chanify@middlebury.edu>
2020-10-01 21:24:38 -0700

    reactstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { Button, Jumbotron } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> 
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <Jumbotron>
+          <h1 className="App-title">
+            <span role="img" aria-label="UmbrellaFire">☔🔥☔</span>
+          </h1>
+          <p className="App-description">
+            Connecting Founders to PNW based investors.
+          </p>
+          <div>
+            <Button
+            color= "primary"
+            size="lg"
+            active
+            class="btn btn-primary"
+            href="https://airtable.com/shrFam7a6yzHCLXk2"
+            target="_blank">
+              Founders
+            </Button>{' '}
+
+
+            <Button
+            color= "primary"
+            size="lg"
+            active
+            class="btn btn-primary"
+            href="https://airtable.com/shrrJDuLj2R9hINCt"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+              Investors
+            </Button>
+          </div>
+        </Jumbotron>
       </header>
     </div>
   );
